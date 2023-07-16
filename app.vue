22913f847ccd540fcea442b7af6701d7619bd702
hare-systems-ryo <ryo@hare-systems.net>
2023-07-16 14:15:13 +0900

    no message

--- app.vue
+++ app.vue
@@ -1,16 +1,38 @@
+<script setup lang="ts">
+useHead({
+  htmlAttrs: { lang: () => 'ja', prefix: 'og: http://ogp.me/ns#' },
+  meta: [
+    { 'http-equiv': 'refresh', content: '' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1.0,maximum-scale=1.0' },
+    { name: 'keywords', content: () => 'keywords' },
+    { name: 'format-detection', content: 'telephone=no' },
+    { name: 'theme-color', content: '#FFF' },
+    { hid: 'robots', name: 'robots', content: 'noindex' },
+  ],
+  link: [],
+  script: [],
+  noscript: [{ children: 'JavaScript is required' }],
+  title: '',
+  bodyAttrs: {
+    class: () => 'lang-ja',
+  },
+});
+</script>
 <template>
-  <div class="test1">
-    <div class="test2">aaaaa</div>
-    <v-btn variant="flat" color="error">btn</v-btn>
+  <div class="px-6 pt-10">
+    <v-container class="bg-white">
+      <div class="">aaaaa</div>
+      <div class="mt-3">
+        <v-text-field label="Label"></v-text-field>
+      </div>
+      <div class="mt-3">
+        <v-textarea label="Label"></v-textarea>
+      </div>
+      <div class="mt-3 d-flex">
+        <v-btn variant="flat" color="primary" class="ms-auto">btn</v-btn>
+      </div>
+    </v-container>
   </div>
 </template>
 
-<style lang="scss" scoped>
-.test1 {
-  background-color: rgb(255, 239, 239);
-  padding: 10px;
-  .test2 {
-    background-color: rgb(185, 185, 255);
-  }
-}
-</style>
+<style lang="scss" scoped></style>
