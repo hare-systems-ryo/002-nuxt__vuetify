8ef8a70f0d6a2de94d8a5f624dfca512315adb38
hare-systems-ryo <ryo@hare-systems.net>
2023-07-16 13:54:11 +0900

    1st

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="test1">
+    <div class="test2">aaaaa</div>
+    <v-btn variant="flat" color="error">btn</v-btn>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.test1 {
+  background-color: rgb(255, 239, 239);
+  padding: 10px;
+  .test2 {
+    background-color: rgb(185, 185, 255);
+  }
+}
+</style>
